db8063963c7bdffcce97a05d845781162cede3fa
sSalvi <jaosalvi@protonmail.com>
2023-02-25 22:49:54 -0300

    Initial commit

---
+++ RenameMovie/RenameMovie/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System;
+
+
+public static class DirExt
+{
+    public static void RenameDir()
+    {
+        string[] dirs = Directory.GetDirectories(Directory.GetCurrentDirectory());
+        char[] numb = {'1', '2'};
+        foreach (string dir in dirs)
+        {
+            string old_name = dir;
+            //Remove pontos
+            string space_name = old_name.Replace('.', ' ');
+            //Remove do ano pra frente
+            int pos = space_name.IndexOfAny(numb);
+            if (pos >= 0) 
+            {
+                string new_name = space_name.Remove(pos);
+                Directory.Move(old_name, new_name);
+            }
+        }
+    }
+    static void Main(string[] args)
+    {
+        RenameDir();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
