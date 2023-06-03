19a6f34924843193860bc1ab6c905235ad616f66
sSalvi <jaosalvi@protonmail.com>
2023-06-03 12:48:29 -0300

    YTS-Updated
    Com o finado RARBG, essa versão funciona pro YTS

--- RenameMovie/RenameMovie/Program.cs
+++ RenameMovie/RenameMovie/Program.cs
@@ -7,17 +7,15 @@ public static class DirExt
     static void Main(string[] args)
     {
         string[] dirs = Directory.GetDirectories(Directory.GetCurrentDirectory());
-        char[] numb = { '1', '2' };
+        char[] numb = { '(', ')' };
         foreach (string dir in dirs)
         {
             string old_name = dir;
-            //Remove pontos
-            string space_name = old_name.Replace('.', ' ');
             //Remove do ano pra frente
-            int pos = space_name.IndexOfAny(numb);
+            int pos = old_name.IndexOfAny(numb);
             if (pos >= 0)
             {
-                string new_name = space_name.Remove(pos);
+                string new_name = old_name.Remove(pos);
                 Directory.Move(old_name, new_name);
             }
         }
